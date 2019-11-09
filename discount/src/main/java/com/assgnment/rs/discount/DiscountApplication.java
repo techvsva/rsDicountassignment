587fe9ad3f9ff67ca8dccf26e0bf0082c03a41e2
techvsva <techvsva@gmail.com>
2019-11-09 17:04:21 +0530

    cahnges related to war packaging

--- discount/src/main/java/com/assgnment/rs/discount/DiscountApplication.java
+++ discount/src/main/java/com/assgnment/rs/discount/DiscountApplication.java
@@ -2,9 +2,16 @@ package com.assgnment.rs.discount;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class DiscountApplication {
+public class DiscountApplication  extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(DiscountApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(DiscountApplication.class, args);
